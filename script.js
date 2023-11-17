cff04654049528ea6b112b2fdee3f2548e2ccf0e
dhanush3727 <dhanushs4827@gmail.com>
2023-11-17 12:58:38 +0530

    final output

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const show = document.querySelector(".show");
 const input = document.querySelector(".pwd");
+const showPassword = document.querySelector(".show-img");
+const passwordBox = document.querySelector(".pass");
+const front = document.querySelector(".front");
+const back = document.querySelector(".back");
+const signUp = document.querySelector(".new-user span");
+const login = document.querySelector(".old-user span");
 
 show.addEventListener("click", () => {
   if (input.type === "password") {
@@ -7,4 +13,23 @@ show.addEventListener("click", () => {
   } else {
     input.type = "password";
   }
+});
+showPassword.addEventListener("click", () => {
+  if (passwordBox.type === "password") {
+    passwordBox.type = "text";
+  } else {
+    passwordBox.type = "password";
+  }
+});
+signUp.addEventListener("click", () => {
+  front.style.zIndex = "1";
+  back.style.zIndex = "2";
+  front.style.transform = "rotateY(180deg)";
+  back.style.transform = "rotateY(0deg)";
+});
+login.addEventListener("click", () => {
+  front.style.zIndex = "2";
+  back.style.zIndex = "1";
+  front.style.transform = "rotateY(0deg)";
+  back.style.transform = "rotateY(180deg)";
 });
